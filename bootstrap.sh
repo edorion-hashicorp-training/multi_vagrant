7388348d2ad073a3a14b0873edf91fb08baf1b54
Andrew McDonald <amcdonald@hashicorp.com>
2021-06-09 15:13:33 +1000

    add dynamic host html page creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,16 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant /var/www
 fi
 
+## print servers IP address
 tput setaf 4; echo "The IP of the host $(hostname) is $(hostname -I | awk '{print $2}')"
+tput setaf 4; echo "To verify please browse to http://$(hostname -I | awk '{print $2}')/$(hostname).html
+
+## generate server identified html file
+cat <<EOF > /var/www/$(hostname).html
+<html>
+<body>
+<p>hostname is: $(hostname)</p>
+</body>
+</html>
+EOF
+
